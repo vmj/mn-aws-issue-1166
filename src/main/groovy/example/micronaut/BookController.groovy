b26c0e60f8fb9825c63b2941c5b5b3228b7bea1b
Mikko Värri <mikko@varri.fi>
2021-09-07 22:32:03 +0300

    add security

--- src/main/groovy/example/micronaut/BookController.groovy
+++ src/main/groovy/example/micronaut/BookController.groovy
@@ -4,9 +4,12 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
 import javax.validation.Valid
 import groovy.transform.CompileStatic
+import io.micronaut.security.annotation.Secured
+import static io.micronaut.security.rules.SecurityRule.IS_AUTHENTICATED
 
 @CompileStatic
 @Controller
+@Secured(IS_AUTHENTICATED)
 class BookController {
 
     @Post
